9b4a61afd9199a645fe39ad481c4fecf7706ef7d
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-10-05 20:53:45 +0800

    Update ClearCommand.java
    Added a GUI to the ClearCommand class such that a warning will promp out upon the command clear is given.

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -5,19 +5,50 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
 /**
+ * Warn the user with a prompt window before the actual clear command executed.
  * Clears the address book.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
+    // new message added to indicate if the address book is already empty
+    public static final String MESSAGE_UNNECESSARY = "Address book is already empty!";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_FAILURE = "Address book is not cleared!";
 
-
+    /**
+     * Function execute will create a warning panel to give the user one more chance
+     * to reconsider before clearing all the data.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return Object CommandResult with the specific message
+     */
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (model.size() == 0) {
+            return new CommandResult(MESSAGE_UNNECESSARY);
+        } else {
+            // confirm that the address book is not empty
+            assert model.size() > 0;
+            //create GUI will the choice of clearing the data
+            JFrame frame = new JFrame();
+            int result = JOptionPane.showConfirmDialog(frame, "Are you sure?\n All data will be cleared!", "WARNING",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+            // if yes is picked, the address book data in model will be reset
+            if(result ==JOptionPane.YES_OPTION) {
+                model.setAddressBook(new AddressBook());
+                return new CommandResult(MESSAGE_SUCCESS);
+            } else {
+                return new CommandResult(MESSAGE_FAILURE);
+            }
+        }
     }
+
 }
