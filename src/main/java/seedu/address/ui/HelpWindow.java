2a3b44d2e6ad135b6e56c5c2440e960a289cacbc
yin72257 <ruohangyin@gmail.com>
2021-10-05 10:56:04 +0800

    Change link to project website

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -16,7 +16,7 @@ import seedu.address.commons.core.LogsCenter;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL =
-            "https://github.com/AY2122S1-CS2103T-W10-4/tp/blob/master/docs/UserGuide.md";
+            "https://ay2122s1-cs2103t-w10-4.github.io/tp/UserGuide.html";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
