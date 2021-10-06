b0013f44a6736ab621c628f0a5788ead01882a81
Banana3021 <anaravi21@gmail.com>
2021-10-06 12:30:04 +0800

    Use value as variable name

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -36,8 +36,8 @@ public class PersonUtil {
         sb.append(PREFIX_PHONE + person.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        sb.append(PREFIX_PRICE + person.getPrice().price + " ");
-        sb.append(PREFIX_INFO + person.getInfo().info + " ");
+        sb.append(PREFIX_PRICE + person.getPrice().value + " ");
+        sb.append(PREFIX_INFO + person.getInfo().value + " ");
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
@@ -53,9 +53,9 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
-        descriptor.getPrice().ifPresent(price -> sb.append(PREFIX_PRICE).append(price.price).append(" "));
+        descriptor.getPrice().ifPresent(price -> sb.append(PREFIX_PRICE).append(price.value).append(" "));
         descriptor.getInfo().ifPresent(imptInfo -> sb.append(PREFIX_ADDRESS).append(
-                imptInfo.info).append(" "));
+                imptInfo.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
