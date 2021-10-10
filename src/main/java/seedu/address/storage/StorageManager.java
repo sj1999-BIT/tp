4ce8416ba94b647a5664efd8c376cf2718072c8f
DesmondTo <e0550619@u.nus.edu>
2021-10-10 20:28:05 +0800

    Countdown: Handle file initiation

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyCountdown;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
 
@@ -18,14 +19,17 @@ public class StorageManager implements Storage {
 
     private static final Logger logger = LogsCenter.getLogger(StorageManager.class);
     private AddressBookStorage addressBookStorage;
+    private CountdownStorage countdownStorage;
     private UserPrefsStorage userPrefsStorage;
 
     /**
      * Creates a {@code StorageManager} with the given {@code AddressBookStorage} and {@code UserPrefStorage}.
      */
-    public StorageManager(AddressBookStorage addressBookStorage, UserPrefsStorage userPrefsStorage) {
+    public StorageManager(AddressBookStorage addressBookStorage, CountdownStorage countdownStorage,
+                          UserPrefsStorage userPrefsStorage) {
         super();
         this.addressBookStorage = addressBookStorage;
+        this.countdownStorage = countdownStorage;
         this.userPrefsStorage = userPrefsStorage;
     }
 
@@ -74,6 +78,36 @@ public class StorageManager implements Storage {
     public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
         logger.fine("Attempting to write to data file: " + filePath);
         addressBookStorage.saveAddressBook(addressBook, filePath);
+    }
+
+
+    // ================ Countdown methods ==============================
+
+    @Override
+    public Path getCountdownFilePath() {
+        return countdownStorage.getCountdownFilePath();
+    }
+
+    @Override
+    public Optional<ReadOnlyCountdown> readCountdown() throws DataConversionException, IOException {
+        return readCountdown(countdownStorage.getCountdownFilePath());
+    }
+
+    @Override
+    public Optional<ReadOnlyCountdown> readCountdown(Path filePath) throws DataConversionException, IOException {
+        logger.fine("Attempting to read data from file: " + filePath);
+        return countdownStorage.readCountdown(filePath);
+    }
+
+    @Override
+    public void saveCountdown(ReadOnlyCountdown countdown) throws IOException {
+        saveCountdown(countdown, countdownStorage.getCountdownFilePath());
+    }
+
+    @Override
+    public void saveCountdown(ReadOnlyCountdown countdown, Path filePath) throws IOException {
+        logger.fine("Attempting to write to data file: " + filePath);
+        countdownStorage.saveCountdown(countdown, filePath);
     }
 
 }
