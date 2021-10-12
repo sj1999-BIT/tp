992b3d535eb0762863bce436f5cfc33d1c27dd0e
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-10-12 21:55:53 +0800

    Added new feature - Group Command
    Able to add tags to a group of contacts at the same time.
    
    Input name must be exact to the stored contact name, else the command stops and return the result.
    
    Updated Model class, Person class, AddCommandTest class and AddressBookParser class such that they support searching of person using only the string name

---
+++ src/main/java/seedu/address/logic/parser/GroupCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.logic.commands.GroupCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class GroupCommandParser implements Parser<GroupCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the GroupCommand
+     * and returns a GroupCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public GroupCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, GroupCommand.MESSAGE_USAGE));
+        }
+
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_TAG, PREFIX_NAME);
+        String nameKeywords = argMultimap.getValue(PREFIX_NAME).get();
+
+        String tag = argMultimap.getValue(PREFIX_TAG).get();
+        return new GroupCommand(tag, nameKeywords);
+    }
+}
