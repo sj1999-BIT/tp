2c5efc34977ac162c67a1572d7c387193e4b2ba2
DesmondTo <e0550619@u.nus.edu>
2021-10-20 09:41:00 +0800

    Add support for price sum of multiple tags

--- src/main/java/seedu/address/logic/parser/PriceCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PriceCommandParser.java
@@ -4,10 +4,12 @@ import static seedu.address.logic.commands.PriceCommand.MESSAGE_ARGUMENT_USAGE;
 import static seedu.address.logic.commands.PriceCommand.MESSAGE_TAG_USAGE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.Arrays;
+import java.util.List;
+
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.PriceCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new PriceCommand object
@@ -30,14 +32,22 @@ public class PriceCommandParser implements Parser<PriceCommand> {
             throw new ParseException(MESSAGE_ARGUMENT_USAGE);
         }
 
+        List<String> tagNameList = getTagNames(args);
+
+
+        return new PriceCommand(tagNameList);
+    }
+
+    private List<String> getTagNames(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
-        String tagName = argMultimap.getValue(PREFIX_TAG).get();
-        String trimmedTag = tagName.trim();
+        String tagNames = argMultimap.getValue(PREFIX_TAG).get();
+        String trimmedTag = tagNames.trim();
         if (trimmedTag.isEmpty()) {
             throw new ParseException(MESSAGE_TAG_USAGE);
         }
 
-        Tag tag = ParserUtil.parseTag(tagName);
-        return new PriceCommand(tag);
+        List<String> tagNameList = Arrays.asList(tagNames.split(" "));
+
+        return tagNameList;
     }
 }
