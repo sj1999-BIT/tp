cb513f12e350f766aa4c87a49e1aa76ec5e359b5
yin72257 <ruohangyin@gmail.com>
2021-11-04 12:40:42 +0800

    Add coverage for findCommandParser

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -8,7 +8,11 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.person.Price;
 import seedu.address.model.person.predicates.NameContainsKeywordsPredicate;
+import seedu.address.model.person.predicates.PriceEqualsNumberPredicate;
+import seedu.address.model.person.predicates.PriceGreaterThanNumberPredicate;
+import seedu.address.model.person.predicates.TagContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -20,7 +24,27 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_emptyTag_throwsParseException() {
+        assertParseFailure(parser, " t/", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyPrice_throwsParseException() {
+        assertParseFailure(parser, " pr/", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.PRICE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidPriceOperator_throwsParseException() {
+        assertParseFailure(parser, " pr/1.00", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.PRICE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidPriceNumber_throwsParseException() {
+        assertParseFailure(parser, " pr/=1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.PRICE_USAGE));
+    }
+
+    @Test
+    public void parse_validNameArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")),
@@ -29,6 +53,55 @@ public class FindCommandParserTest {
 
         // multiple whitespaces between keywords
         assertFindCommandParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validTagArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new TagContainsKeywordsPredicate(Arrays.asList("friends")),
+                        "tag is in [friends] ");
+        assertFindCommandParseSuccess(parser, " t/friends", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validPriceEqualArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PriceEqualsNumberPredicate(new Price("0.00")), "price is =0.00");
+        assertFindCommandParseSuccess(parser, " pr/=0.00", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validPriceGreaterThanArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PriceGreaterThanNumberPredicate(new Price("0.00")), "price is >0.00");
+        assertFindCommandParseSuccess(parser, " pr/>0.00", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validPriceLessThanArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PriceEqualsNumberPredicate(new Price("0.00")), "price is <0.00");
+        assertFindCommandParseSuccess(parser, " pr/<0.00", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validPriceLessThanEqualArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PriceEqualsNumberPredicate(new Price("0.00")), "price is <=0.00");
+        assertFindCommandParseSuccess(parser, " pr/<=0.00", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validPriceGreaterThanEqualArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PriceEqualsNumberPredicate(new Price("0.00")), "price is >=0.00");
+        assertFindCommandParseSuccess(parser, " pr/>=0.00", expectedFindCommand);
     }
 
 }
