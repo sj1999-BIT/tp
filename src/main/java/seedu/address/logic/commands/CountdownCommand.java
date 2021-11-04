3cbe07702244ba48ec39520f16a96eabd46273d8
DesmondTo <e0550619@u.nus.edu>
2021-11-04 20:13:22 +0800

    Remove unused method

--- src/main/java/seedu/address/logic/commands/CountdownCommand.java
+++ src/main/java/seedu/address/logic/commands/CountdownCommand.java
@@ -45,17 +45,6 @@ public class CountdownCommand extends Command {
         this.dateSetByUser = dateSetByUser;
     }
 
-    /**
-     * Returns the day count based on the wedding date set by user.
-     *
-     * @param model The app model used to retrieve the wedding date set by user.
-     * @return the day count based on the wedding date set by user.
-     */
-    public static int getCountdown(Model model) {
-        LocalDate weddingDate = model.getCountdown().getDate();
-        return (int) LocalDate.now().until(weddingDate, ChronoUnit.DAYS);
-    }
-
     /** Calculates and shows the day count before the wedding day. */
     private CommandResult executeShowCountDown(Model model) {
         LocalDate weddingDate = model.getCountdown().getDate();
